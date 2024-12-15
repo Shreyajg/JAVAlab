3265b9f23ee4a800f84b402bab7bdf4dc92c5e05
Shreyajg <shreyajaganatha.ci23@bmsce.ac.in>
2024-12-15 22:01:11 +0530

    Update Externals.java

--- week5/Externals.java
+++ week5/Externals.java
@@ -1,28 +1,49 @@
-// File: CIE/Student.java
-package CIE;
+// File: SEE/Externals.java
+package SEE;
 
+import CIE.Internals;  // Import Internals class from CIE package
 import java.util.Scanner;
 
-public class Student {
-    protected String usn;
-    protected String name;
-    protected int sem;
+public class Externals extends Internals {
+    protected int externalMarks[] = new int[5];  // Array to store external marks for 5 courses
+    protected int finalMarks[] = new int[5];     // Array to store final marks (sum of internal and external)
 
-    // Method to input student details
-    public void inputStudentDetails() {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter USN: ");
-        this.usn = s.nextLine();
-        System.out.print("Enter Name: ");
-        this.name = s.nextLine();
-        System.out.print("Enter Semester: ");
-        this.sem = s.nextInt();
+    // Constructor to initialize arrays
+    public Externals() {
+        externalMarks = new int[5];
+        finalMarks = new int[5];
     }
 
-    // Method to display student details
-    public void displayStudentDetails() {
-        System.out.println("USN: " + usn);
-        System.out.println("Name: " + name);
-        System.out.println("Semester: " + sem);
+    // Method to input external marks
+    public void inputSEEmarks() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter External Marks for 5 courses: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Course " + (i + 1) + ": ");
+            externalMarks[i] = s.nextInt();
+        }
+    }
+
+    // Method to calculate final marks (sum of internal and external)
+    public void calculateFinalMarks() {
+        for (int i = 0; i < 5; i++) {
+            finalMarks[i] = marks[i] + externalMarks[i];  // Internal + External
+        }
+    }
+
+    // Method to display final marks
+    public void displayFinalMarks() {
+        displayStudentDetails();  // Display student details (from parent class)
+        displayCIEmarks();        // Display internal marks (from parent class)
+       
+        System.out.println("External Marks: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Course " + (i + 1) + ": " + externalMarks[i]);
+        }
+
+        System.out.println("Final Marks: ");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Course " + (i + 1) + ": " + finalMarks[i]);
+        }
     }
 }
